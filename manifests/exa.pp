f480403b9529166a11613f8559c5599a8d07000f
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-23 16:56:18 -0400

    Attempting to order the classes' execution

--- manifests/exa.pp
+++ manifests/exa.pp
@@ -2,4 +2,6 @@ class role::exa {
   include profile::base
   include profile::domain_member
   include profile::exchange
+  #Ordering execution
+  Class['profile::base'] -> Class['profile::domain_member'] -> Class['profile::exchange']
 }
