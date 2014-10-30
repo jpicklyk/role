ca217c5dbeb190b6accd2081e85325d51365ee15
Jeff Picklyk <jeff.picklyk@sta.samsung.com>
2014-10-30 13:42:38 -0400

    Structuring the ordering according to Gary

--- manifests/dc.pp
+++ manifests/dc.pp
@@ -1,5 +1,6 @@
 class role::dc {
-  contain profile::base  # All roles should have the base profile
-  contain profile::active_directory
-  contain profile::dns
+  include profile::base  # All roles should have the base profile
+  include profile::active_directory
+  include profile::dns
+  Class['profile::base'] -> Class['profile::active_directory'] -> Class['profile::dns']
 }
